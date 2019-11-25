742ddcc98cd57b771480a5878fef976c810fd6bf
ishica23 <isaac-higareda@hotmail.com>
2019-11-25 13:45:25 -0600

    first commit

---
+++ pythontest1.py
@@ -0,0 +1,48 @@
+import random
+
+hangman=""
+angman="---"
+angman="| "
+angman=" | |"
+angman=" | O"
+
+
+
+
+def loadWordFromDoc():
+    try:
+        word=""
+        index=0
+
+        print (randomNumber)
+        with open ('C:\\Users\\pc caeshica\\Documents\\palabras.txt',"r") as f:
+            for line in f:
+                index=index + 1
+                if randomNumber == index:
+                    word=line
+                    break
+    except FileNotFoundError as error:
+        print("error"+error)    
+    return word
+
+
+def guessTheLetter():
+    global attempts
+    while (True):
+        palabra=loadWordFromDoc()
+        letter=input("what is the letter: ")
+        print (palabra)
+        print (letter)
+        for i in range(countAttempts):
+            if letter == palabra.strip():
+                print ("Congratulations guessed the word")
+                break;
+            else:
+                attempts= attempts -i
+                print ("try again you got "+ str(attempts))
+                letter=input("what is the letter")
+    
+randomNumber=random.randrange(1,3)
+countAttempts=3
+attempts=3
+guessTheLetter()
